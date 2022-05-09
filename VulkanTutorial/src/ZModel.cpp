48db0addcea17fbd0550486cdaa1f28c3b66803e
Zixin Zhang <zzhng151@illinois.edu>
2022-05-08 21:11:37 -0400

    Index and Staging Buffers - Vulkan Game Engine Tutorial 16

--- VulkanTutorial/src/ZModel.cpp
+++ VulkanTutorial/src/ZModel.cpp
@@ -31,10 +31,11 @@ namespace ZZX
 		return attributeDescriptions;
 	}
 
-	ZModel::ZModel(ZDevice& zDevice, const std::vector<Vertex>& vertices)
+	ZModel::ZModel(ZDevice& zDevice, const ZModel::Builder& builder)
 		: m_zDevice(zDevice)
 	{
-		createVertexBuffers(vertices);
+		createVertexBuffers(builder.vertices);
+		createIndexBuffers(builder.indices);
 	}
 
 	ZModel::~ZModel()
@@ -42,6 +43,12 @@ namespace ZZX
 		// TODO: this has limitations. Consider using library like VMA 
 		vkDestroyBuffer(m_zDevice.device(), m_vertexBuffer, nullptr);
 		vkFreeMemory(m_zDevice.device(), m_vertexBufferMemory, nullptr);
+
+		if (m_hasIndexBuffer)
+		{
+			vkDestroyBuffer(m_zDevice.device(), m_indexBuffer, nullptr);
+			vkFreeMemory(m_zDevice.device(), m_indexBufferMemory, nullptr);
+		}
 	}
 
 	void ZModel::bind(VkCommandBuffer commandBuffer)
@@ -49,11 +56,22 @@ namespace ZZX
 		VkBuffer buffers[] = {m_vertexBuffer};
 		VkDeviceSize offsets[] = {0};
 		vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+		if (m_hasIndexBuffer)
+		{
+			vkCmdBindIndexBuffer(commandBuffer, m_indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+		}
 	}
 
 	void ZModel::draw(VkCommandBuffer commandBuffer)
 	{
-		vkCmdDraw(commandBuffer, m_vertexCount, 1, 0, 0);
+		if (m_hasIndexBuffer)
+		{
+			vkCmdDrawIndexed(commandBuffer, m_indexCount, 1, 0, 0, 0);
+		}
+		else
+		{
+			vkCmdDraw(commandBuffer, m_vertexCount, 1, 0, 0);
+		}
 	}
 
 	void ZModel::createVertexBuffers(const std::vector<Vertex>& vertices)
@@ -61,15 +79,62 @@ namespace ZZX
 		m_vertexCount = static_cast<uint32_t>(vertices.size());
 		assert(m_vertexCount >= 3 && "vertex count must be at least 3");
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
 		m_zDevice.createBuffer(bufferSize,
-		                       VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+		                       VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
 		                       VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-		                       m_vertexBuffer,
-		                       m_vertexBufferMemory);
+		                       stagingBuffer,
+		                       stagingBufferMemory);
 		void* data;
-		vkMapMemory(m_zDevice.device(), m_vertexBufferMemory, 0, bufferSize, 0, &data);
+		vkMapMemory(m_zDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
 		// how mpmcpy works here: https://youtu.be/mnKp501RXDc?t=1041
 		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-		vkUnmapMemory(m_zDevice.device(), m_vertexBufferMemory);
+		vkUnmapMemory(m_zDevice.device(), stagingBufferMemory);
+
+		m_zDevice.createBuffer(bufferSize,
+		                       VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+		                       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+		                       m_vertexBuffer,
+		                       m_vertexBufferMemory);
+		m_zDevice.copyBuffer(stagingBuffer, m_vertexBuffer, bufferSize);
+
+		vkDestroyBuffer(m_zDevice.device(), stagingBuffer, nullptr);
+		vkFreeMemory(m_zDevice.device(), stagingBufferMemory, nullptr);
+	}
+
+	void ZModel::createIndexBuffers(const std::vector<uint32_t>& indices)
+	{
+		m_indexCount = static_cast<uint32_t>(indices.size());
+		m_hasIndexBuffer = m_indexCount > 0;
+		if (!m_hasIndexBuffer)
+		{
+			return;
+		}
+		VkDeviceSize bufferSize = sizeof(indices[0]) * m_indexCount;
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		m_zDevice.createBuffer(bufferSize,
+		                       VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+		                       VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+		                       stagingBuffer,
+		                       stagingBufferMemory);
+		void* data;
+		vkMapMemory(m_zDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		// how mpmcpy works here: https://youtu.be/mnKp501RXDc?t=1041
+		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(m_zDevice.device(), stagingBufferMemory);
+
+		m_zDevice.createBuffer(bufferSize,
+		                       VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+		                       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+		                       m_indexBuffer,
+		                       m_indexBufferMemory);
+		m_zDevice.copyBuffer(stagingBuffer, m_indexBuffer, bufferSize);
+
+		vkDestroyBuffer(m_zDevice.device(), stagingBuffer, nullptr);
+		vkFreeMemory(m_zDevice.device(), stagingBufferMemory, nullptr);
 	}
 }
--- VulkanTutorial/src/ZModel.h
+++ VulkanTutorial/src/ZModel.h
@@ -19,7 +19,13 @@ namespace ZZX
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 		};
 
-		ZModel(ZDevice& zDevice, const std::vector<Vertex>& vertices);
+		struct Builder
+		{
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
+
+		ZModel(ZDevice& zDevice, const ZModel::Builder& builder);
 		~ZModel();
 
 		// delete copy ctor and assignment to avoid dangling pointer
@@ -30,9 +36,17 @@ namespace ZZX
 		void draw(VkCommandBuffer commandBuffer);
 	private:
 		void createVertexBuffers(const std::vector<Vertex>& vertices);
+		void createIndexBuffers(const std::vector<uint32_t>& indices);
+
 		ZDevice& m_zDevice;
+
 		VkBuffer m_vertexBuffer;
 		VkDeviceMemory m_vertexBufferMemory;
 		uint32_t m_vertexCount;
+
+		bool m_hasIndexBuffer = false;
+		VkBuffer m_indexBuffer;
+		VkDeviceMemory m_indexBufferMemory;
+		uint32_t m_indexCount;
 	};
 };
